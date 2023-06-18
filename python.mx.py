cb33cceffe1e97317f5ed04914db4ff06b16e49f
Muxammadsodiq <120007009+MuxammadsodiqDev@users.noreply.github.com>
2023-06-18 22:29:20 +0500

    biron nima
    rostanam biron nima

---
+++ python.mx.py
@@ -0,0 +1,64 @@
+'''otam={'ism':'Matnazar', 't_yil':1978, 'ish':'molyarchik'}
+ism=otam['ism']
+t_yil=otam['t_yil']
+ish=otam['ish']
+print(f'Otamni ismi {ism},\
+ {t_yil} yilda tug\'ilganlar,\
+ {ish} bo\'lip ishlidila')'''
+'''sevimli_toamlar={'otam':'manti','onam':'barak','otajon':'shashlik', 'latif':'tovuq'}
+print(f"Otamning sevimli toami {sevimli_toamlar['otam']}  ")
+print(f"Onamning sevimli toami {sevimli_toamlar['onam']}")
+print(f"Otajonning sevimli toami {sevimli_toamlar['otajon']}")
+print(f"Latifning sevimli toami {sevimli_toamlar['latif']}")
+'''
+
+
+
+
+'''izohli_lugat={"integer":"butun sonlar toplami",
+              "string":'unicodedagi borcha belgilar qabul qiladi',
+              'float':'barcha kars qiymatlarni qabul qiladi',
+              'if':"agar shart operatori",
+              'else':"aks holda",
+              "elif":'agar aks holda',
+              "for":'uchun sikl',
+              "and":'va ifni ichida u sharthamn bu shart ham ro\' berishiga aytiladi',
+              'or':'yoki u yoki bu shartlar bajarilishidan kelib chiqadi',
+              'list': 'ro\'yhat ',
+              'tuple':'o\'zgarmas ro\'yhat',
+              'dictionary':'kalit so\'z va qiymatni o\'z ichiga olgan ro\'yhat'}
+print(izohli_lugat)'''
+
+
+
+
+
+'''lugat={'water':'suv', 'wight':'og\'ir','love':'sevgi',"I":'men',"you":'sen'}
+soz=input('iltimos eng so\'z kiriting: ')
+if soz in lugat:
+    tarjima=lugat[soz]
+    print(soz,' tarjimasi' ,tarjima)
+else:
+    print('bunday so\'z mavjud emas')'''
+    
+    
+    
+#print(python_izohli_lugati)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
